84addcc8862a4fa6acc7e1408abdda6e20b44039
Pranjal <pranjalkishor5@gmail.com>
2024-11-10 12:21:24 +0530

    Matcher made

---
+++ matcher.py
@@ -0,0 +1,43 @@
+import pickle
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+with open('linkage_matrix.pkl', 'rb') as f:
+    Z = pickle.load(f)
+
+with open('cluster_assignments.pkl', 'rb') as f:
+    cluster_assignments = pickle.load(f)
+
+data = pd.read_csv('medimatch_data.csv')
+
+def get_most_similar_medicine(query_medicine):
+    query_cluster = data[data['Medicine Name'] == query_medicine]['Cluster'].values[0]
+    
+    cluster_meds = data[data['Cluster'] == query_cluster]
+    
+    cluster_meds_processed = cluster_meds.drop(columns=['Medicine Name', 'Cluster'])
+    
+    scaler = StandardScaler()
+    cluster_meds_processed_scaled = scaler.fit_transform(cluster_meds_processed)
+    
+
+    query_index = cluster_meds[cluster_meds['Medicine Name'] == query_medicine].index[0]
+    
+
+    nn = NearestNeighbors(n_neighbors=2, metric='euclidean') 
+    nn.fit(cluster_meds_processed_scaled)
+    
+    distances, indices = nn.kneighbors([cluster_meds_processed_scaled[query_index]])
+    
+
+    closest_medicine_index = indices[0][1]  
+    closest_medicine = cluster_meds.iloc[closest_medicine_index]['Medicine Name']
+    
+    return closest_medicine
+
+
+query_medicine = "Avastin 400mg Injection"  # Replace with any medicine name
+most_similar_medicine = get_most_similar_medicine(query_medicine)
+print(f"The most similar medicine to {query_medicine} is {most_similar_medicine}.")
